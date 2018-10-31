709c8813ca5e6a172282b2fd922b7776615d1d96
Gregory Yentz <gyentz@gmail.com>
2018-10-31 13:35:21 -0400

    10/31 13:35

---
+++ Assignment_7/DivideByZeroWithExceptionHandling.java
@@ -0,0 +1,48 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class DivideByZeroWithExceptionHandling{
+	public static int quotient(int numerator, int denominator)
+		throws ArithmeticException
+	{
+		return numerator/denominator;
+	}
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		boolean continueLoop = true;
+		int numerator=0;
+		int denominator=0;
+
+		do{
+			try{
+				System.out.print("Please enter an integer numerator: ");
+				numerator = scanner.nextInt();
+				System.out.print("Please enter an integer denominator: ");
+				denominator = scanner.nextInt();
+				if(denominator==0){
+					throw new ArithmeticException("Oops, can't do that.");
+				}
+
+				int result = quotient(numerator, denominator);
+				System.out.printf("%nResult: %d / %d = %d%n", numerator, denominator, result);
+				continueLoop = false;
+			}
+			catch(InputMismatchException inputMismatchException){
+				System.err.printf("%nException: %s%n", inputMismatchException);
+				scanner.nextLine();
+				System.out.printf("You must enter integers. Please try again.%n%n");
+			}
+			catch(ArithmeticException arithmeticException){
+				System.err.printf("%nException: %s%n", arithmeticException);
+				System.out.printf("Zero is an invalid denominator. Please try again.%n%n");
+			}
+			finally{
+				System.out.printf("%nNumerator is %d%nDenominator is %d%n%n", numerator, denominator);
+
+
+			}
+
+		} while(continueLoop);
+	}
+}
